cd3ab8a9355375287b04fecb43b1f237fe5983cc
Pete Heaney <peteheaney@gmail.com>
2019-10-29 19:24:55 +0000

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,10 @@ const config = {
     entry: './src/input-soft-limit.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'input-soft-limit.js'
+        filename: 'input-soft-limit.js',
+        library: 'inputSoftLimit',
+        libraryTarget: 'umd',
+        umdNamedDefine: true
     },
     module: {
         rules: [
